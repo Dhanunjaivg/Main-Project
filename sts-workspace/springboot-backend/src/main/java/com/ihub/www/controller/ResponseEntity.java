50eafaf7d29b2954212db845e5855f3fd9888444
dhanunjai <dhanunjaivanga37@gmail.com>
2024-12-10 15:10:13 +0530

    My-Backend

---
+++ sts-workspace/springboot-backend/src/main/java/com/ihub/www/controller/ResponseEntity.java
@@ -0,0 +1,12 @@
+package com.ihub.www.controller;
+
+import com.ihub.www.model.Employee;
+
+public class ResponseEntity {
+
+	public static ResponseEntity ok(Employee updateEmp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
